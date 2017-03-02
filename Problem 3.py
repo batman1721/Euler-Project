3b2a5ed4ec32c3362c5f3729f6463f223c66175a
batman1721 <jm.goncalves.92@hotmail.com>
2017-03-02 10:32:48 +0000

    Add files via upload

--- Problem 3.py
+++ Problem 3.py
@@ -3,14 +3,31 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
-from isPrime2 import is_prime
-number=input('Number: ')
+import math
 
-for i in xrange(number,1,-1):
-    if i%2==0:
-        pass
-    elif number%i==0:
-        print i
-        if is_prime(i)==True:
-            print i
-            break
+n=input('Number: ')
+
+if n%2==0:
+    lfactor=2
+    n=n/2
+    while n%2==0:
+        n=n/2
+else:
+    lfactor=1
+
+factor=3
+maxfactor=math.sqrt(n)
+
+while (n>1 and factor<=maxfactor):
+    if n%factor==0:
+        n=n/factor
+        lfactor=factor
+        while n%factor==0:
+            n=n/factor
+        maxfactor=math.sqrt(n)
+    factor=factor+2
+
+if n==1:
+    print lfactor
+else:
+    print n
